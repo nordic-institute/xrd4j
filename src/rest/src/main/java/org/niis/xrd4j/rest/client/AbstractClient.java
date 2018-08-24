f5f91719cd111b6811fb8c5b5fe3540eead0920a
Janne Mattila <janne.mattila@gofore.com>
2018-08-24 09:33:37 +0300

    XRDDEV-81 configurable proxy

--- src/rest/src/main/java/org/niis/xrd4j/rest/client/AbstractClient.java
+++ src/rest/src/main/java/org/niis/xrd4j/rest/client/AbstractClient.java
@@ -32,13 +32,10 @@ import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.Map;
 
 /**
@@ -50,10 +47,8 @@ import java.util.Map;
 public abstract class AbstractClient implements RESTClient {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AbstractClient.class);
-    public static final int HOVERFLY_PROXY_PORT = 8500;
-    public static final int SQUID_PROXY_PORT = 3128;
-    public static final int PROXY_PORT = HOVERFLY_PROXY_PORT;
-    public static final boolean USE_PROXY = "true".equalsIgnoreCase(System.getProperty("useProducerProxy"));
+
+    private RequestConfig requestConfig = null; // null unless proxy is set
 
     /**
      * @param url URL where the request is sent
@@ -63,6 +58,18 @@ public abstract class AbstractClient implements RESTClient {
      * @return new HttpUriRequest object
      */
     protected abstract HttpUriRequest buildtHttpRequest(String url, String requestBody, Map<String, String> headers, RequestConfig config);
+
+    /**
+     * Configures requestConfig so that a http proxy is used for requests
+     * @param hostName
+     * @param port
+     */
+    public void setProxy(String hostName, int port) {
+        HttpHost proxy = new HttpHost(hostName, port, "http");
+        requestConfig = RequestConfig.custom()
+                .setProxy(proxy)
+                .build();
+    }
 
     /**
      * Makes a HTTP request to the given URL using the given request body,
@@ -80,32 +87,9 @@ public abstract class AbstractClient implements RESTClient {
     public ClientResponse send(String url, String requestBody, Map<String, ?> params, Map<String, String> headers) {
         // Build target URL
         url = ClientUtil.buildTargetURL(url, params);
-        // Build request
-        RequestConfig config = null;
-        if (USE_PROXY && (!url.contains("Consumer") && !url.contains("Provider"))) {
-            // use only for outgoing requests, temp hack above ^
-            URL parsedUrl;
-            try {
-                parsedUrl = new URL(url);
-            } catch (MalformedURLException e) {
-                throw new RuntimeException("Cannot determine proxy request scheme, malformed URL: " + url, e);
-            }
-            LOGGER.info("using proxy, sending request to: " + url + " with protocol: " + parsedUrl.getProtocol());
-            LOGGER.info("trustStore: {}", System.getProperty("javax.net.ssl.trustStore"));
-            LOGGER.info("trustStoreType: {}", System.getProperty("javax.net.ssl.trustStoreType"));
-            LOGGER.info("trustStorePassword: {}", System.getProperty("javax.net.ssl.trustStorePassword"));
-//            HttpHost proxy = new HttpHost("127.0.0.1", PROXY_PORT, parsedUrl.getProtocol());
-            HttpHost proxy = new HttpHost("127.0.0.1", PROXY_PORT, "http");
-            HttpHost proxy2 = new HttpHost("127.0.0.1", PROXY_PORT);
-            config = RequestConfig.custom()
-                    .setProxy(proxy)
-                    .build();
-            LOGGER.info("request body: " + requestBody);
-        } else {
-            LOGGER.info("not using proxy for: " + url);
-        }
+        LOGGER.debug("proxy: {} for url: {}", (requestConfig != null), url);
 
-        HttpUriRequest request = this.buildtHttpRequest(url, requestBody, headers, config);
+        HttpUriRequest request = this.buildtHttpRequest(url, requestBody, headers, requestConfig);
 
         LOGGER.info("Starting HTTP {} operation.", request.getMethod());
 
@@ -119,7 +103,6 @@ public abstract class AbstractClient implements RESTClient {
 
 
         try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
-//        try (CloseableHttpClient httpClient = createClient(url.startsWith("https"))) {
 
             //Send the request; It will immediately return the response in HttpResponse object
             CloseableHttpResponse response = httpClient.execute(request);
@@ -138,10 +121,6 @@ public abstract class AbstractClient implements RESTClient {
             // Get response payload
             String responseStr = ClientUtil.getResponseString(response.getEntity());
 
-            if (USE_PROXY) {
-                LOGGER.info("response body from proxy: " + responseStr);
-            }
-
             response.close();
             httpClient.close();
             LOGGER.debug("REST response content type: \"{}\".", contentType);
@@ -156,34 +135,4 @@ public abstract class AbstractClient implements RESTClient {
             return null;
         }
     }
-
-//    private CloseableHttpClient createClient(boolean ssl) {
-//        if (ssl) {
-//            LOGGER.info("creating trust-all-certs HttpClient");
-//            try {
-//                TrustStrategy trustStrategy = new TrustStrategy() {
-//                    @Override
-//                    public boolean isTrusted(X509Certificate[] chain, String authType) {
-//                        return true;
-//                    }
-//                };
-//                HostnameVerifier hostnameVerifier = new HostnameVerifier() {
-//                    @Override
-//                    public boolean verify(String hostname, SSLSession session) {
-//                        return true;
-//                    }
-//                };
-//                return HttpClients.custom()
-//                        .setSSLSocketFactory(new SSLConnectionSocketFactory(
-//                                new SSLContextBuilder().loadTrustMaterial(trustStrategy).build(),
-//                                hostnameVerifier))
-//                        .build();
-//            } catch (Exception e) {
-//                throw new RuntimeException(e);
-//            }
-//        } else {
-//            LOGGER.info("creating default HttpClient");
-//            return HttpClients.createDefault();
-//        }
-//    }
 }
