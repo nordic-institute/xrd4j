5d3247da9eb58e229fbd806ade13787616ee56cf
Pauline Dimmek <ma74reso@studserv.uni-leipzig.de>
2025-05-21 12:37:27 +0300

    refactor

--- src/common/build.gradle.kts
+++ src/common/build.gradle.kts
@@ -2,9 +2,6 @@ plugins {
     id("xrd4j.java-conventions")
 }
 
-group="org.niis.xrd4j"
-version="1.0-SNAPSHOT"
-
 dependencies {
     api(libs.com.sun.xml.messaging.saaj.saajImpl)
     api(libs.org.slf4j.slf4jApi)
